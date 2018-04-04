b9a640045a4ea51597765493ddd81b8b2dc1fe63
agaszab <gatito@poczta.pl>
2018-04-04 07:55:18 +0200

    budżet domowy

---
+++ src/main/java/pl/javastart/zad22web/Opcje.java
@@ -0,0 +1,25 @@
+package pl.javastart.zad22web;
+
+public enum Opcje {
+    DODAJ_PRZYCHOD (1,"Dodanie nowego przychodu"),
+    DODAJ_WYDATEK (2,"Dodanie nowego wydatku"),
+    POKAZ_PRZYCHODY (3,"Wyświetlenie wszystkich przychodów"),
+    POKAZ_WYDATKI (4,"Wyświetlenie wszystkich wydatków"),
+    KONIEC (5, "Koniec programu");
+
+    private final String name;
+    private final int nr;
+
+    Opcje(int i, String name) {
+        this.name = name;
+        this.nr=i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNr() {
+        return nr;
+    }
+}
---
+++ src/main/java/pl/javastart/zad22web/PrzychodyController.java
@@ -0,0 +1,71 @@
+package pl.javastart.zad22web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+
+
+@Controller
+public class PrzychodyController {
+
+    private Przychody przychody;
+
+    public PrzychodyController(Przychody przychody) {
+        this.przychody = przychody;
+    }
+
+    public PrzychodyController() {
+    }
+
+    @RequestMapping("/")
+    public static String main() {
+        return "index";
+    }
+
+
+    @RequestMapping("/list")
+    public String opcje(Model model, @RequestParam Opcje opcja) throws SQLException {
+
+        PrzychodyDao dao = new PrzychodyDao();
+
+        switch (opcja) {
+            case DODAJ_PRZYCHOD: {
+                przychody = new Przychody();
+                przychody.setType("przychody");
+                model.addAttribute("przychody", przychody);
+                return "formularz";
+            }
+            case DODAJ_WYDATEK: {
+                przychody = new Przychody();
+                przychody.setType("wydatki");
+                model.addAttribute("przychody", przychody);
+                return "formularz";
+            }
+            case POKAZ_PRZYCHODY: {
+                dao.show("przychody");
+                break;
+            }
+            case POKAZ_WYDATKI: {
+                dao.show("wydatki");
+                break;
+            }
+
+
+            //model.addAttribute("przychody", przychody);
+
+        }
+        return "lista";
+
+    }
+
+    @RequestMapping("/dodaj")
+    public String dodajPrzychody(Model model, @RequestParam String opcje){
+        przychody=new Przychody();
+        przychody.setType(opcje);
+        model.addAttribute("przychody", przychody);
+        return "formularz";
+    }
+}
